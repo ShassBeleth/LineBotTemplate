33110167d9cf35042e7118bc9240efd5710fddf3
ShassBeleth <shassbeleth@gmail.com>
2017-07-26 01:07:38 +0900

    Initial LINE Bot Template

---
+++ LineBotTemplate/LineBotTemplate/Controllers/WebhookController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace LineBotTemplate.Controllers {
+
+	/// <summary>
+	/// LINE BotからのWebhook送信先API
+	/// </summary>
+	public class WebhookController : ApiController {
+
+		/// <summary>
+		/// POSTメソッド
+		/// </summary>
+		/// <param name="requestToken">リクエストトークン</param>
+		/// <returns>常にステータス200のみを返す</returns>
+		public async Task<HttpResponseMessage> Post( JToken requestToken ) => new HttpResponseMessage( HttpStatusCode.OK );
+
+	}
+
+}
